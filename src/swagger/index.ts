df81d9b82f624d061712499c9a94fc7ca36a8067
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-06-24 14:20:48 +0500

    build(deps): update 1 file and create 1 file

---
+++ src/swagger/index.ts
@@ -0,0 +1,45 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+// import { version } from '../package.json'
+
+const app: Express = express();
+
+const options: swaggerJsdoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Minglr API Docs",
+            version: "1.0.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            // license: {
+            //   name: "MIT",
+            //   url: "https://spdx.org/licenses/MIT.html",
+            // },
+            // contact: {
+            //   name: "LogRocket",
+            //   url: "https://logrocket.com",
+            //   email: "info@email.com",
+            // },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.ts"],
+};
+
+const specs = swaggerJsdoc(options);
+
+export function swaggerDocs(app: Express, port: number) {
+    app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(specs, {
+            explorer: true,
+        })
+    );
+}
