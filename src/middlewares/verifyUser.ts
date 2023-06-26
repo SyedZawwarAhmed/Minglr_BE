1bb90060658ee891e2f0b8aafc3ff9a1b81e830d
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-06-27 01:18:48 +0500

    middlewares added

--- src/middlewares/verifyUser.ts
+++ src/middlewares/verifyUser.ts
@@ -2,19 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET_KEY } from "../configs/env.config";
 
-export function verifyUser(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization
-  const token = authHeader?.split(" ")[1]
+function verifyUser(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
   jwt.verify(
     token as string,
     JWT_SECRET_KEY as string,
     (error: any, decode: any): any => {
       if (error) {
-        return next(error)
+        return next(error);
       } else {
-        res.locals.user = decode
-        return next()
+        res.locals.user = decode;
+        return next();
       }
     }
   );
 }
+
+export default verifyUser;
