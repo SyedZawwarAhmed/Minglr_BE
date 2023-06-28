158df7fa2ac7eb0436454210cdf8a780ab4f98cb
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-06-28 14:47:52 +0500

    update db.config.ts and db.service.ts

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,11 +1,9 @@
-import pool from "../configs/db.config";
+import { pool } from "../configs/db.config";
 
-async function query(sql: string, params: Array<any>) {
+export async function query(sql: string, params: Array<any>) {
   const connection = await pool.getConnection();
   const [results]: any = await connection.query(sql, params);
   connection.release();
 
   return results;
 }
-
-export default query;
