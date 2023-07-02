904cc6d47d8d015e5f230981514e70bcb6fc1aff
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-07-03 00:44:38 +0500

    build(deps): update package-lock.json, package.json and src/index.ts (setup cors)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Express, NextFunction, Request, Response } from "express";
+import cors from "cors"
 import { swaggerDocs } from "./swagger";
 import { userRouter, postRouter } from "./routes";
 
 const app: Express = express();
 const port = 3000;
+
+app.use(cors())
 
 app.use(express.json());
 app.use(
