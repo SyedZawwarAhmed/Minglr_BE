644315140374beb203f19558247f0403e0cc0fd9
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-07-10 20:25:37 +0500

    update post.service.ts

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -19,16 +19,20 @@ export async function getAll({ page, limit }: getAllPostsDataInterface) {
   const userIds = results.map((post: any) => post.user_id);
   const uniqueUserIds = [...new Set(userIds)];
 
-  const uniqueUsers: any = await Promise.all(uniqueUserIds.map(
-    async (id: any) =>
-      (await query(
-        `SELECT id, first_name, last_name, picture_url FROM users WHERE id = ?`,
-        [id]
-      ))[0]
-  ));
+  const uniqueUsers: any = await Promise.all(
+    uniqueUserIds.map(
+      async (id: any) =>
+        (
+          await query(
+            `SELECT id, first_name, last_name, picture_url FROM users WHERE id = ?`,
+            [id]
+          )
+        )[0]
+    )
+  );
 
   results.forEach((post: any) => {
-    post.author = uniqueUsers.find((user: any) => user.id === post.user_id)
+    post.author = uniqueUsers.find((user: any) => user.id === post.user_id);
   });
 
   return getResponseObject("Posts of Signed in User.", { posts: results });
@@ -86,24 +90,33 @@ export async function comment(commentData: CommentDataInterface) {
 }
 
 export async function getComments(getCommentsData: GetCommentsDataInterface) {
-  const { postId, page, limit } = getCommentsData
+  const { postId, page, limit } = getCommentsData;
   const offset = getOffset(parseInt(page), parseInt(limit));
-  const results: any = await query("SELECT * FROM comments WHERE post_id = ? ORDER BY id DESC LIMIT ? OFFSET ?", [postId, parseInt(limit), offset])
+  const results: any = await query(
+    "SELECT * FROM comments WHERE post_id = ? ORDER BY id DESC LIMIT ? OFFSET ?",
+    [postId, parseInt(limit), offset]
+  );
 
   const userIds = results.map((post: any) => post.user_id);
   const uniqueUserIds = [...new Set(userIds)];
 
-  const uniqueUsers: any = await Promise.all(uniqueUserIds.map(
-    async (id: any) =>
-      (await query(
-        `SELECT id, first_name, last_name, picture_url FROM users WHERE id = ?`,
-        [id]
-      ))[0]
-  ));
+  const uniqueUsers: any = await Promise.all(
+    uniqueUserIds.map(
+      async (id: any) =>
+        (
+          await query(
+            `SELECT id, first_name, last_name, picture_url FROM users WHERE id = ?`,
+            [id]
+          )
+        )[0]
+    )
+  );
 
   results.forEach((post: any) => {
-    post.author = uniqueUsers.find((user: any) => user.id === post.user_id)
+    post.author = uniqueUsers.find((user: any) => user.id === post.user_id);
   });
 
-  return getResponseObject(`All Comments on post of id ${postId}`, { comments: results });
-} 
+  return getResponseObject(`All Comments on post of id ${postId}`, {
+    comments: results,
+  });
+}
